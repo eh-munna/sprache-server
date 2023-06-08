9646ed5a458e86bef005af7ae60bc60580048ea2
ehmunna <emran-hussain.munna-tu-ilmenau.de>
2023-06-09 00:38:53 +0200

    cors added

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middleware
+
+// cors middleware
+
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
 // basic route testing
 
 app.get('/', (req, res) => {
